9800ebf6ac91110756b30cd4b82835e7a9f356c8
Kamva-Ntlanga <240497821@mycput.ac.za>
2026-05-09 18:54:01 +0200

    Update staff_account_repository.py

--- repositories/staff_account_repository.py
+++ repositories/staff_account_repository.py
@@ -1,1 +1,21 @@
+from typing import List, Optional, abstractmethod
+from repositories.repository_interface import Repository
+from src.staff_account import StaffAccount
 
+class StaffAccountRepository(Repository[StaffAccount, str]):
+    """Repository interface for StaffAccount entities."""
+    
+    @abstractmethod
+    def find_by_username(self, username: str) -> Optional[StaffAccount]:
+        """Find staff by username"""
+        pass
+    
+    @abstractmethod
+    def find_by_role(self, role: str) -> List[StaffAccount]:
+        """Find all staff with a specific role"""
+        pass
+    
+    @abstractmethod
+    def find_active_staff(self) -> List[StaffAccount]:
+        """Find all active staff accounts"""
+        pass
