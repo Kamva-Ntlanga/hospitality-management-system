1a3389576e2c77404e5887c97f67f48b57978b2d
Kamva-Ntlanga <240497821@mycput.ac.za>
2026-05-09 19:23:00 +0200

    Update test_repository_factory.py

--- tests/test_repository_factory.py
+++ tests/test_repository_factory.py
@@ -1,1 +1,21 @@
+import unittest
+from factories.repository_factory import RepositoryFactory, StorageType
 
+class TestRepositoryFactory(unittest.TestCase):
+    def test_create_room_repository_memory(self):
+        repo = RepositoryFactory.create_room_repository(StorageType.MEMORY)
+        from repositories.inmemory.in_memory_room_repository import InMemoryRoomRepository
+        self.assertIsInstance(repo, InMemoryRoomRepository)
+    
+    def test_create_guest_repository_memory(self):
+        repo = RepositoryFactory.create_guest_repository(StorageType.MEMORY)
+        from repositories.inmemory.in_memory_guest_repository import InMemoryGuestRepository
+        self.assertIsInstance(repo, InMemoryGuestRepository)
+    
+    def test_factory_returns_different_instances(self):
+        repo1 = RepositoryFactory.create_room_repository(StorageType.MEMORY)
+        repo2 = RepositoryFactory.create_room_repository(StorageType.MEMORY)
+        self.assertIsNot(repo1, repo2)
+
+if __name__ == "__main__":
+    unittest.main()
