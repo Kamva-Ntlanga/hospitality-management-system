f2e5b3eba45a2a80e11e1ec8b0e1b68f86fa4fb0
Kamva-Ntlanga <240497821@mycput.ac.za>
2026-05-09 18:52:11 +0200

    Update guest_repository.py

--- repositories/guest_repository.py
+++ repositories/guest_repository.py
@@ -1,1 +1,16 @@
+from typing import List, Optional, abstractmethod
+from repositories.repository_interface import Repository
+from src.guest import Guest
 
+class GuestRepository(Repository[Guest, str]):
+    """Repository interface for Guest entities."""
+    
+    @abstractmethod
+    def find_by_email(self, email: str) -> Optional[Guest]:
+        """Find a guest by email address"""
+        pass
+    
+    @abstractmethod
+    def find_by_loyalty_points_range(self, min_points: int, max_points: int) -> List[Guest]:
+        """Find guests with loyalty points in range"""
+        pass
